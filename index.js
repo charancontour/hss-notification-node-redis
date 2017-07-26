a2c1e64c140ac087f76f06b974f9bcaa53aafc3a
charan <charan@contourgroupinc.com>
2017-07-26 21:18:59 +0530

    initial push

---
+++ index.js
@@ -0,0 +1,31 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var Redis = require('ioredis');
+var redis = new Redis();
+redis.subscribe('notifications');
+
+redis.on('message',function(channel,message){
+    console.log(message);
+    message = JSON.parse(message);
+    io.emit(channel+'-'+message.user_id,message);
+});
+
+app.get('/', function(req, res){
+res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
