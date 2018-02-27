96fdb42ef3f9da635a1e96400a3cd1053b83c284
charan <charan@contourgroupinc.com>
2018-02-27 13:08:50 +0530

    added channel for case mangement

--- index.js
+++ index.js
@@ -3,16 +3,23 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var Redis = require('ioredis');
 var redis = new Redis();
-redis.subscribe('notifications','project');
+redis.subscribe('notifications','project','case_project');
 
 redis.on('message',function(channel,message){
     message = JSON.parse(message);
     if(channel == 'notifications'){
       io.emit(message.app_name+'-'+channel+'-'+message.user_id,message);
-    }else if(channel == 'project') {
+    } else if(channel == 'project') {
       for (var i = 0; i < message.data.users.length; i++) {
         io.emit(message.app_name+'-'+channel+'-'+message.event+'-'+message.data.users[i].id,message.data.data);
       }
+      // io.emit(channel+'-'+message.event,message.data);
+    } else if(channel == 'case_project') {
+      for (var i = 0; i < message.data.users.length; i++) {
+        io.emit(message.app_name+'-'+channel+'-'+message.event+'-'+message.data.users[i].id,message.data.data);
+        // console.log(message.app_name+'-'+channel+'-'+message.event+'-'+message.data.users[i].id);
+      }
+      // console.log(message);
       // io.emit(channel+'-'+message.event,message.data);
     }
 
